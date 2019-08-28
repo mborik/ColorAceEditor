a02547bc712654fe00575d815237627b72cd31cb
mborik128 <mborik@users.sourceforge.net>
2019-08-28 18:32:14 +0200

    fixed redrawRect calculations

--- src/editor/ActionHandler.ts
+++ src/editor/ActionHandler.ts
@@ -33,28 +33,28 @@ export class ActionHandler {
 			const { x, y } = editor.translateCoords(e.pageX, e.pageY);
 
 			switch (editor.editTool) {
-				case EditorTool.Selection:
+				case EditorTool.Selection: {
 					editor.selection.reset(x, y);
 					editor.scroller.zoomTo(editor.zoomFactor);
 					break;
-
-				case EditorTool.GridSelect:
+				}
+				case EditorTool.GridSelect: {
 					editor.selection.reset(Math.floor(x / 6) * 6, y);
 					editor.scroller.zoomTo(editor.zoomFactor);
 					break;
-
-				case EditorTool.Pencil:
+				}
+				case EditorTool.Pencil: {
 					editor.draw.dot(x, y);
 					break;
-
-				case EditorTool.Lines:
+				}
+				case EditorTool.Lines: {
 					// secret weapon for point selection
 					if (!editor.coordsRecorder.some(v => (v.x === x && v.y === y))) {
 						editor.coordsRecorder.push({ x, y });
 						editor.draw.dot(x, y);
 					}
 					break;
-
+				}
 				case EditorTool.Rectangle:
 					this.actionSnapshot = editor.pixel.doSnapshot(true);
 					break;
@@ -84,23 +84,23 @@ export class ActionHandler {
 			const { x, y } = editor.translateCoords(e.pageX, e.pageY);
 
 			switch (editor.editTool) {
-				case EditorTool.Selection:
+				case EditorTool.Selection: {
 					if (!this.mouseNotMoved) {
 						editor.pixel.redrawSelection(s => {
 							s.set(s.x0, s.y0, x - 1, y - 1);
 						});
 					}
 					break;
-
-				case EditorTool.GridSelect:
+				}
+				case EditorTool.GridSelect: {
 					if (!this.mouseNotMoved) {
 						editor.pixel.redrawSelection(s => {
 							s.set(s.x0, s.y0, (Math.ceil(x / 6) * 6) - 1, y - 1);
 						});
 					}
 					break;
-
-				case EditorTool.Pencil:
+				}
+				case EditorTool.Pencil: {
 					if (this.lastPixelX !== x || this.lastPixelY !== y) {
 						editor.draw.line(
 							this.lastPixelX,
@@ -110,14 +110,18 @@ export class ActionHandler {
 						);
 					}
 					break;
-
-				case EditorTool.Rectangle:
-					this.drawRectangle(
+				}
+				case EditorTool.Rectangle: {
+					editor.pixel.undo(this.actionSnapshot);
+					editor.draw.rectangle(
 						this.startPixelX,
 						this.startPixelY,
 						x, y, editor.editFilled
 					);
+
+					this.redrawRect(x, y);
 					break;
+				}
 			}
 
 			this.lastPixelX = x;
@@ -136,7 +140,7 @@ export class ActionHandler {
 			const { x, y } = editor.translateCoords(e.pageX, e.pageY);
 
 			switch (editor.editTool) {
-				case EditorTool.Selection:
+				case EditorTool.Selection: {
 					if (!this.mouseNotMoved) {
 						editor.pixel.redrawSelection(s => {
 							s.set(s.x0, s.y0, x - 1, y - 1);
@@ -145,8 +149,8 @@ export class ActionHandler {
 
 					editor.selectionActionCallback(editor.selection.nonEmpty());
 					break;
-
-				case EditorTool.GridSelect:
+				}
+				case EditorTool.GridSelect: {
 					if (!this.mouseNotMoved) {
 						editor.pixel.redrawSelection(s => {
 							s.set(s.x0, s.y0, (Math.ceil(x / 6) * 6) - 1, y - 1);
@@ -155,22 +159,26 @@ export class ActionHandler {
 
 					editor.selectionActionCallback(editor.selection.nonEmpty());
 					break;
-
-				case EditorTool.Pencil:
+				}
+				case EditorTool.Pencil: {
 					if (!this.mouseNotMoved &&
 						(this.lastPixelX !== x || this.lastPixelY !== y)) {
 
 						editor.draw.line(this.lastPixelX, this.lastPixelY, x, y, true);
 					}
 					break;
-
-				case EditorTool.Rectangle:
-					this.drawRectangle(
+				}
+				case EditorTool.Rectangle: {
+					editor.pixel.undo(this.actionSnapshot);
+					editor.draw.rectangle(
 						this.startPixelX,
 						this.startPixelY,
 						x, y, editor.editFilled
 					);
+
+					this.redrawRect(x, y);
 					break;
+				}
 			}
 		}
 
@@ -178,7 +186,7 @@ export class ActionHandler {
 		this.mouseBtnFlag = 0;
 	}
 
-	@debounce(16)
+	@debounce(16, { leading: true })
 	mouseWheel(e: React.WheelEvent) {
 		let delta = 0;
 		if (e.deltaY > 0) {
@@ -193,28 +201,38 @@ export class ActionHandler {
 		}
 	}
 
-	private drawRectangle(x1: number, y1: number, x2: number, y2: number, filled: boolean) {
-		editor.pixel.undo(this.actionSnapshot);
-		editor.draw.rectangle(x1, y1, x2, y2, filled);
+	private redrawRect(x2: number, y2: number) {
+		let x1: number, y1: number;
 
-		if (x1 > this.lastPixelX) {
-			x2 = x1;
-			x1 = this.lastPixelX;
+		if (this.startPixelX > x2) {
+			x1 = x2;
+			x2 = this.startPixelX;
 		} else {
-			x2 = this.lastPixelX;
+			x1 = this.startPixelX;
 		}
 
-		if (y1 > this.lastPixelY) {
-			y2 = y1;
-			y1 = this.lastPixelY;
+		if (this.startPixelY > y2) {
+			y1 = y2;
+			y2 = this.startPixelY;
 		} else {
+			y1 = this.startPixelY;
+		}
+
+		if (this.lastPixelX > x2) {
+			x2 = this.lastPixelX;
+		} else if (this.lastPixelX < x1) {
+			x1 = this.lastPixelX;
+		}
+		if (this.lastPixelY > y2) {
 			y2 = this.lastPixelY;
+		} else if (this.lastPixelY < y1) {
+			y1 = this.lastPixelY;
 		}
 
 		x1 = Math.floor(x1 / 6) * 6;
 		x2 = Math.ceil(++x2 / 6) * 6;
 		y1 = (y1 & ~1);
-		y2 = (++y2 & ~1) + 2;
+		y2 = (y2 & ~1) + 2;
 
 		editor.pixel.redrawRect(x1, y1, (x2 - x1), (y2 - y1), true);
 	}
